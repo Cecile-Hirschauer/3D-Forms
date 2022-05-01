4895d9650893366810702e0f0bd3d5c88844adec
cecil <ola@example.com>
2022-05-01 10:37:45 +0200

    Finished project

---
+++ script.js
@@ -0,0 +1,16 @@
+const signUpBtn = document.querySelector(".signup-btn");
+const signInBtn = document.querySelector(".signin-btn");
+const formsWrapper = document.querySelector(".forms-wrapper");
+
+
+signUpBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formsWrapper.classList.add("change");
+  // console.log(formsWrapper);
+  
+});
+
+signInBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formsWrapper.classList.remove("change");
+});
